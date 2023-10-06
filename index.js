638dd054f56966b33fee14b946bcdb0dbd3ce816
Diego Garrido <dgarridoal@gmail.com>
2023-10-06 14:13:17 -0300

    Proyecto Inicial

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+// const { bdConnect } = require("./Config/database");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// bdConnect();
+app.use("/api", require("./app/routes/index"));
+
+app.listen(PORT, () => {
+  console.log("Server status ---> OK ", PORT);
+});
